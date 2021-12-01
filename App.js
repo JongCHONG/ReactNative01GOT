7e6d97e8354e32879bf97d7bef34a7693ba88a28
JongCHONG <yonhoa27@hotmail.fr>
2021-12-01 17:02:22 +0100

    modal

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { Image, FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Modal, Image, FlatList, Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   const [listCharacters, setListCharacters] = useState()
+  const [modalVisible, setModalVisible] = useState(false)
 
   useEffect(() => {
     fetch("https://thronesapi.com/api/v2/Characters")
@@ -11,29 +12,54 @@ export default function App() {
       .then(data => setListCharacters(data))
   },[])
 
+  const Character = ({ item }) => {
+
+    return (
+      <>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <Text>{item.fullName}</Text>
+          <Text>{item.family}</Text>
+          <Pressable
+            style={[styles.button, styles.buttonClose]}
+            onPress={() => setModalVisible(!modalVisible)}
+          >
+            <Text style={styles.textStyle}>Hide Modal</Text>
+          </Pressable>
+        </Modal>
+        <Pressable onPress={() => setModalVisible(true)}>
+          <View style={styles.imageContainer}>
+            <Text>{item.fullName}</Text>
+            <Image
+              style={styles.image}
+              source={{
+                uri: item.imageUrl
+              }}
+            />
+          </View>
+        </Pressable>
+      </>
+    )
+  }
+
   console.log(listCharacters)
   return (
+    <>
       <View style={styles.container}>
         <FlatList data={listCharacters} renderItem={Character} />
-      <Text>Test</Text>
-      <StatusBar style="auto" />
-    </View>
+      </View>
+    </>
   );
 }
 
-const Character = ({ item }) => {
-  return (
-    <View style={styles.imageContainer}>
-      <Text>{item.fullName}</Text>
-      <Image
-        style={styles.image}
-        source={{
-          uri: item.imageUrl
-        }}
-      />
-    </View>
-  )
-}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -46,7 +72,20 @@ const styles = StyleSheet.create({
     width: 200,
     height: 200
   },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
   imageContainer: {
     marginBottom: 10
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
   },
 });
