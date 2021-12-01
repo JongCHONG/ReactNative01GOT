e877ad39a24ece103de2d20e585e3f328e980e60
JongCHONG <yonhoa27@hotmail.fr>
2021-12-01 15:59:47 +0100

    list characters

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Image, FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [listCharacters, setListCharacters] = useState()
+
+  useEffect(() => {
+    fetch("https://thronesapi.com/api/v2/Characters")
+      .then(response => response.json())
+      .then(data => setListCharacters(data))
+  },[])
+
+  console.log(listCharacters)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container}>
+        <FlatList data={listCharacters} renderItem={Character} />
+      <Text>Test</Text>
       <StatusBar style="auto" />
     </View>
   );
+}
+
+const Character = ({ item }) => {
+  return (
+    <View style={styles.imageContainer}>
+      <Text>{item.fullName}</Text>
+      <Image
+        style={styles.image}
+        source={{
+          uri: item.imageUrl
+        }}
+      />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +41,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200
+  },
+  imageContainer: {
+    marginBottom: 10
   },
 });
